8e9d7283825dec64eb972e92d12f85b142386345
HisashiHasebe <hhasebe@diverta.co.jp>
2022-04-28 14:16:02 +0800

    owners-page追加

---
+++ pages/owners-page/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{ response.details.ext_col_01}}</h1>
+    <p>{{ response.details.ext_col_02}}</p>
+
+  <button type="button" @click="logout">
+  ログアウト
+  </button>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  async asyncData({$axios}) {
+    try {
+      const response = await $axios.$get(process.env.BASE_URL + '/rcms-api/4/service_top/4')
+      return { response }
+    } catch (e) {
+      console.log(e.message)
+    }
+  },
+  methods: {
+    ...mapActions(['logout'])
+  }
+}
+</script>
