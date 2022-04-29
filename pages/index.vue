a383334e0767cf929afeb90140ac1b6184974a55
HisashiHasebe <hhasebe@diverta.co.jp>
2022-04-29 11:51:05 +0800

    トップページ修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,10 @@
 <template>
+  <main>
   <h1>各ページへのリンク</h1>
   <div><nuxt-link to="/login/">ログインページ</nuxt-link></div>
   <div><nuxt-link to="/news/">ニュース一覧ページ</nuxt-link></div>
   <div><nuxt-link to="/owners-page/">契約者専用ページ</nuxt-link></div>
+  </main>
 </template>
 
 <script>
