7f9c0dd5fabd851c2f30559a5f16fa4300cbd696
HisashiHasebe <hhasebe@diverta.co.jp>
2022-05-17 16:40:56 +0800

    Revert "baseURL指定、withCredentials: true"
    This reverts commit dd1ddfc48dcc0df054b476688f92d3bcb1129393.

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,9 +44,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: process.env.BASE_URL,
-    credentials: true,
-    withCredentials: true
+    baseURL: '/',
+    credentials: true
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
