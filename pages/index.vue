f5e4401ba9453e73f998c9001053248f1f180aa9
HisashiHasebe <hhasebe@diverta.co.jp>
2022-05-17 22:00:19 +0800

    testブランチ

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <main>
   <h1>各ページへのリンク</h1>
+  <p>testブランチ<p/>
   <div><nuxt-link to="/login/">ログインページ</nuxt-link></div>
   <div><nuxt-link to="/news/">ニュース一覧ページ</nuxt-link></div>
   <div><nuxt-link to="/owners-page/">契約者専用ページ</nuxt-link></div>
